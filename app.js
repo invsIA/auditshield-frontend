01f774a04d053595e4a0745e40017fa937857c57
AuditShieldAI <shopyourdream6@gmail.com>
2025-06-25 22:44:52 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-fetch("https://auditshield-backend-569.onrender.com/")
+fetch("https://auditshield-backend.onrender.com/")
   .then(response => response.json())
   .then(data => {
     document.body.innerHTML = `<h1>${data.message}</h1>`;
